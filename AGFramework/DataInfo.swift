42b88038d0a77bdda35d34e2f4464f7b36233645
Malli246 <srinivas.ios2017@gmail.com>
2017-09-13 10:25:38 +0530

    ccc

---
+++ AGFramework/DataInfo.swift
@@ -0,0 +1,16 @@
+//
+//  DataInfo.swift
+//  AGFramework
+//
+//  Created by Vmoksha on 13/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+import Foundation
+
+
+public class DataInfo {
+    
+    
+    
+}
